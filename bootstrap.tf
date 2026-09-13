74a19a24e304ceda9912c12589fb06d003472434
ShumaHidaka <syuu_rikuzyou@yahoo.co.jp>
2026-09-14 08:11:52 +0900

    fix: update GitHub OIDC provider thumbprint 2

--- bootstrap.tf
+++ bootstrap.tf
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a218683b7f75e0c8b8423f06d18253373400"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 # 4. GitHub Actions用 IAMロール
